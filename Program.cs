1a6965bd4806244cbaa2edef83675a48635b2f7d
igorpantelic <igorpantelic@gmail.com>
2022-04-01 20:34:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World 123!");
+
+IConfiguration config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .AddEnvironmentVariables()
+    .Build();
+
+var dbConnectionString = config.GetSection("db").Value;
+
+Console.WriteLine($"Using connection string: {dbConnectionString}");
+
+using (SqlConnection conn = new SqlConnection(dbConnectionString))
+{
+    conn.Open();
+
+    using (SqlCommand cmd = new SqlCommand("select name from sys.databases", conn))
+    {
+        using (SqlDataReader reader = cmd.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                Console.WriteLine(reader.GetString(0));
+            }
+        }
+    }
+
+    Console.WriteLine("\nDone!!!");
+    Console.ReadLine();
+
+}
